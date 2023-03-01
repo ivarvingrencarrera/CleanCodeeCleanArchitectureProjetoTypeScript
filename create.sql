42d9843d69e3388092f09684ee3e805f2587a6e0
IvarCarrera <ivar.carrera@gmail.com>
2023-03-01 15:50:22 -0300

    Parte II do Projeto

--- create.sql
+++ create.sql
@@ -4,16 +4,23 @@ DROP TABLE ecommerce.product;
 CREATE TABLE ecommerce.product (
     id_product integer,
     description text,
-    price numeric
+    price numeric,
+    width integer,
+    height integer,
+    length integer,
+    weight numeric
 );
 
-INSERT INTO ecommerce.product (id_product, description, price) values(1, 'A', 1000);
-INSERT INTO ecommerce.product (id_product, description, price) values(2, 'B', 5000);
-INSERT INTO ecommerce.product (id_product, description, price) values(3, 'C', 30);
+INSERT INTO ecommerce.product (id_product, description, price, width, height, length, weight) values(1, 'A', 1000, 50, 30, 10, 3);
+INSERT INTO ecommerce.product (id_product, description, price, width, height, length, weight) values(2, 'B', 5000, 50, 50, 50, 22);
+INSERT INTO ecommerce.product (id_product, description, price, width, height, length, weight) values(3, 'C', 30, 10, 10, 10, 0.9);
+INSERT INTO ecommerce.product (id_product, description, price, width, height, length, weight) values(4, 'C', 30, -10, 10, 10, 0.9);
 
 CREATE TABLE ecommerce.coupon (
     code text,
-    percentage numeric
+    percentage numeric,
+    expire_date timestamp
 );
 
-INSERT INTO ecommerce.coupon (code, percentage) values('VALE20', 20);
+INSERT INTO ecommerce.coupon (code, percentage, expire_date) values('VALE20', 20, '2023-10-01T10:00:00');
+INSERT INTO ecommerce.coupon (code, percentage, expire_date) values('VALE10', 10, '2022-10-01T10:00:00');
--- src/main.ts
+++ src/main.ts
@@ -5,30 +5,54 @@ const app = express()
 app.use(express.json())
 
 app.post("/checkout", async function (req: Request, res: Response) {
-	const output: Output = {
-		total: 0
-	}
-	const connection = pgp()("postgres://postgres:firework@localhost:5432/app")
-	if (req.body.items) {
-		for (const item of req.body.items) {
-			const [productData] = await connection.query("select * from ecommerce.product where id_product = $1", item.idProduct)
-			output.total += parseFloat(productData.price) * item.quantity
-		}
-	}
-	if (req.body.coupon) {
-		const [couponData] = await connection.query("select * from ecommerce.coupon where code = $1", [req.body.coupon])
-		const percentage = parseFloat(couponData.percentage)
-		output.total -= (output.total * percentage)/100
-	}
-	const isValid = validate(req.body.cpf)
-	if (!isValid) output.message = "Invalid cpf"
-	await connection.$pool.end()
-	res.json(output)
+    const connection = pgp()("postgres://postgres:firework@localhost:5432/app")
+	try {
+        const isValid = validate(req.body.cpf)
+        if (!isValid) throw new Error("Invalid CPF")
+        const output: Output = {
+            total: 0,
+            freight: 0
+        }
+        const items: number[] = []
+        if (req.body.items) {
+            for (const item of req.body.items) {
+                if (item.quantity <= 0) throw new Error("Invalid quantity")
+                if (items.includes(item.idProduct)) throw new Error ("Duplicated item")
+                const [productData] = await connection.query("select * from ecommerce.product where id_product = $1", item.idProduct)
+                if (productData.width <= 0 || productData.height <= 0 || productData.length <= 0 || parseFloat(productData.weight) <= 0) throw new Error ("Invalid dimension")
+                output.total += parseFloat(productData.price) * item.quantity
+                const volume = productData.width/100 * productData.height/100 * productData.length/100
+                const density = parseFloat(productData.weight)/volume
+                const itemFreight = 1000 * volume * (density/100)
+                output.freight += Math.max(itemFreight, 10) * item.quantity
+                items.push(item.idProduct)
+            }
+        }
+        if (req.body.coupon) {
+            const [couponData] = await connection.query("select * from ecommerce.coupon where code = $1", [req.body.coupon])
+            if (couponData.expire_date.getTime() > new Date().getTime()) {
+                const percentage = parseFloat(couponData.percentage)
+                output.total -= (output.total * percentage)/100
+            }
+        }
+        if (req.body.from && req.body.to) {
+            output.total += output.freight
+        }
+        res.json(output)
+    }
+    catch (e: any) {
+        res.status(422).json({
+            message: e.message
+        })
+    }
+    finally {
+        await connection.$pool.end()
+    }
 })
 
 type Output = {
 	total: number,
-	message?: string
+    freight: number
 }
 
 app.listen(3000)
